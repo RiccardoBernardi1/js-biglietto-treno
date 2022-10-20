0d14af670f33d9d9caa6f929971dd8cba3f74f6d
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-10-20 13:56:15 +0200

    Aggiunge calcolo prezzo standard

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const km= Number( prompt ("Quanti chilometri devi percorrere?") );
 // 2. chiedere all'utente l'eta del passeggero 
 const age= Number( prompt ("Quanti anni hai?") );
 // 3. calcolare il prezzo standard del biglietto 
+let price= (km*0.21);
 // 4. verificare se il passeggero sia minorenne e abbia quindi diritto ad un 20% sconto
 // 5. verificare se il passegero sia ultrasessantacinquenne e abbia quindi diritto ad un 40% sconto
 // 6. calcolare il prezzo finale 
