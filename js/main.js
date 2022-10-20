2e0d727815d6aca9386dc6ada46791fec9977c8c
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-10-20 15:32:09 +0200

    Aggiunge struttura html e stile css

--- js/main.js
+++ js/main.js
@@ -7,8 +7,12 @@ let price= (km*0.21);
 // 4. verificare se il passeggero sia minorenne e abbia quindi diritto ad un 20% sconto o sia ultrasessantacinquenne e abbia quindi diritto ad un 40% sconto
 if (age<18){
     price= ( price-(price*0.2) );
+    document.getElementById("discount").innerHTML= "Sconto minorenni : " +parseFloat(price*0.2).toFixed(2)+"€";
 }else if (age>=65){
     price= ( price-(price*0.4) );
+    document.getElementById("discount").innerHTML= "Sconto over-65 : "+parseFloat(price*0.2).toFixed(2)+"€";
+}else {
+    document.getElementById("discount").style.visibility = "hidden";
 }
 // 5. mostrare a schermo il prezzo finale con massimo due decimali 
-document.querySelector("h2").innerHTML="Questo è il prezzo del tuo biglietto:"+parseFloat(price).toFixed(2)+"€"; 
+document.getElementById("final-price").innerHTML= parseFloat(price).toFixed(2)+"€"; 
