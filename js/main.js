ed7d082796cbefe7991c167355d708854952109a
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-10-20 13:46:32 +0200

    Crea struttura base progetto

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// 1. chiedere all'utente i km da percorrere 
+// 2. chiedere all'utente e l'eta del passeggero 
+// 3. calcolare il prezzo standard del biglietto 
+// 4. verificare se il passeggero sia minorenne e abbia quindi diritto ad un 20% sconto
+// 5. verificare se il passegero sia ultrasessantacinquenne e abbia quindi diritto ad un 40% sconto
+// 6. calcolare il prezzo finale 
+// 7. mostrare a schermo il prezzo finale con massimo due decimali 
