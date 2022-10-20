0531ee9473183f0de75dba6b55384ccfe7a8f21e
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-10-20 13:52:55 +0200

    Aggiunge prompt per eta e km da percorrere

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 // 1. chiedere all'utente i km da percorrere 
-// 2. chiedere all'utente e l'eta del passeggero 
+const km= Number( prompt ("Quanti chilometri devi percorrere?") );
+// 2. chiedere all'utente l'eta del passeggero 
+const age= Number( prompt ("Quanti anni hai?") );
 // 3. calcolare il prezzo standard del biglietto 
 // 4. verificare se il passeggero sia minorenne e abbia quindi diritto ad un 20% sconto
 // 5. verificare se il passegero sia ultrasessantacinquenne e abbia quindi diritto ad un 40% sconto
